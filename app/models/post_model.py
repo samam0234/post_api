74138b698d77b85f68aa451d81bd9b3927747c44
while <limjinheng0120@gmail.com>
2026-06-23 23:42:16 +0900

    feat: PostStat 테이블 추가 및 Post 1:1 양방향 참조 - post_stat 테이블 (like_count 등) - Post.stat ↔ PostStat.post 관계 설정
    Attachment 테이블 추가 및 Post 1:N 양방향 참조
    - attachment 테이블 (filename 등)
    - Post.attachments ↔ Attachment.post 관계 설정
    - cascade delete-orphan 적용

--- app/models/post_model.py
+++ app/models/post_model.py
@@ -3,7 +3,8 @@
 # 역할: DB 테이블 구조만 정의합니다. 쿼리 로직은 여기 없습니다.
 # ============================================================
 
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.database import Base
 
@@ -22,3 +23,40 @@ class Post(Base):
         onupdate=datetime.now,   # UPDATE 실행 시 자동으로 현재 시각 갱신
         comment="수정 시각"
     )
+
+    stat = relationship(
+        "PostStat",
+        back_populates="post",
+        uselist=False,  # 1대 1
+        cascade="all, delete-orphan"
+    ) # 이 객체가 PostStat 객체를 속성으로 가지고 있지 않으면 가지고 있지 못한다. 그러니 속성값을 가지고 있어야 한다.
+
+    Attachment = relationship(
+        "Attachment",
+        back_populates="post",
+        uselist=True,   # 1대 다
+        cascade="all, delete-orphan"    # 부모 삭제시 첨부파일도 자동 삭제
+    )
+
+class PostStat(Base) :
+    __tablename__ = "post_stat"
+    
+    # unique=True → 1대1 보장 (post - PostStat의 관계가 1:1)
+    id         = Column(Integer, primary_key=True, autoincrement=True)
+    post_id    = Column(Integer, ForeignKey("post.id"),
+                        unique=True, nullable=False)
+    like_count = Column(Integer, default=0, comment="좋아요 수")
+    created_at = Column(DateTime, default=datetime.now)
+
+    post = relationship("Post", back_populates="stat")  # 이 객체를 가지고 있는 값 : Post
+
+class Attachment(Base):
+    __tablename__ = "attachment"
+
+    # unique=True → 1대N (post - Attachment의 관계가 1:N)
+    id         = Column(Integer, primary_key=True, autoincrement=True)
+    post_id    = Column(Integer, ForeignKey("post.id"), nullable=False, unique=True)
+    filename   = Column(String(255), nullable=False, comment="파일명")
+    created_at = Column(DateTime, default=datetime.now)
+
+    post = relationship("Post", back_populates="attachments")
